1828a322c819aa9ae73b3c0b62bb2039ae7ccdcb
1ntrovertskrrt <21104045@ittelkom-pwt.ac.id>
2024-05-01 23:50:20 +0700

    removed unused import

--- lib/controllers/login_controller.dart
+++ lib/controllers/login_controller.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: non_constant_identifier_names, use_build_context_synchronously
 
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
